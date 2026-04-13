8ba218e4dcf40fd871c3155ac69fc96d368e9852
ramjsandal <61168190+ramjsandal@users.noreply.github.com>
2026-04-13 14:08:56 -0400

    working final

--- rl_books.py
+++ rl_books.py
@@ -1,6 +1,7 @@
 import csv
+import argparse
 import random
-import os 
+import os
 import numpy as np
 import torch
 from torch import nn
@@ -8,6 +9,8 @@ from torch.utils.data import Dataset
 from torch.utils.data import DataLoader
 from torchvision import datasets
 from torchvision.transforms import ToTensor
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 
@@ -22,8 +25,6 @@ with open(filename, 'r', encoding='utf-8') as csvfile:
     for row in csvreader:
         rows.append(row)
 
-    #print("Total no. of rows: %d" % csvreader.line_num)  # Row count
-#print('Field names are: ' + ', '.join(fields))
 random.shuffle(rows)
 
 # clean up data, put fields were going to use as features into
@@ -31,19 +32,37 @@ random.shuffle(rows)
 
 # ratings index = 3
 # integer features indices = 7, 8, 9
+# title 1
+# author 2
+# num_pages 7
+# language_code 6
 
+# numeric features
 features = []
-labels = []
+
+# labels
+average_ratings = []
+
+# text features
+titles = []
+authors = []
+
 for row in rows:
     try:
-        feature = [float(row[7]), float(row[8]), float(row[9])]
-        label = float(row[3])
-        features.append(feature)
-        labels.append(label)
+        # get the numeric features and put them into an array
+        numeric_features = [len(row[1]), len(row[1].split()), float(row[7]), int.from_bytes(row[6].encode('utf-8'), 'big')]
+        features.append(numeric_features)
+
+        # string features we have to deal with later
+        titles.append(row[1])
+        authors.append(row[2]) 
+
+        # labels
+        average_ratings.append(float(row[3]))
+
     except:
         continue
 
-features = np.array(features, dtype=np.float32)
 features = np.array(features, dtype=np.float32)
 features = (features - features.mean(axis=0)) / features.std(axis=0)
 
@@ -65,23 +84,40 @@ class GoodreadsDataset(Dataset):
 
 # should use the first 80% of the list for training
 # and final 20% for testing
+
+title_vectorizer = TfidfVectorizer(max_features=7000) 
+author_vectorizer = CountVectorizer(max_features=500)
+
 dividing_index = int(len(features) * .8)
 train_features = features[:dividing_index]
-train_labels = labels[:dividing_index]
+train_average_ratings = average_ratings[:dividing_index]
+train_titles = titles[:dividing_index]
+train_authors = authors[:dividing_index]
+train_titles_text = title_vectorizer.fit_transform(train_titles).toarray()
+train_authors_text = author_vectorizer.fit_transform(train_authors).toarray()
+
 test_features = features[dividing_index:]
-test_labels = labels[dividing_index:]
+test_average_ratings = average_ratings[dividing_index:]
+test_titles = titles[dividing_index:]
+test_authors = authors[dividing_index:]
+test_titles_text = title_vectorizer.transform(test_titles).toarray()
+test_authors_text = author_vectorizer.transform(test_authors).toarray()
 
-train_dataset = GoodreadsDataset(train_features, train_labels)
-test_dataset = GoodreadsDataset(test_features, test_labels)
+train_features = np.concatenate([train_features, train_titles_text, train_authors_text], axis=1)
+test_features = np.concatenate([test_features, test_titles_text, test_authors_text], axis=1)
 
-train_dataloader = DataLoader(train_dataset, 32, True)
-test_dataloader = DataLoader(test_dataset, 32, False)
+avg_train_dataset = GoodreadsDataset(train_features, train_average_ratings)
+avg_test_dataset = GoodreadsDataset(test_features,  test_average_ratings)
+avg_train_dataloader = DataLoader(avg_train_dataset, 32, True)
+avg_test_dataloader = DataLoader(avg_test_dataset, 32, False)
 
-class NeuralNetwork(nn.Module):
+class AllTogether(nn.Module):
     def __init__(self):
         super().__init__()
         self.linear_relu_stack = nn.Sequential(
-            nn.Linear(3, 128),
+            nn.Linear(4 + 7000 + 500, 256),
+            nn.ReLU(),
+            nn.Linear(256, 128),
             nn.ReLU(),
             nn.Linear(128, 64),
             nn.ReLU(),
@@ -95,11 +131,10 @@ class NeuralNetwork(nn.Module):
         return logits
 
 device = "cpu"
-model = NeuralNetwork().to(device=device)
-print(model)
+avg_model = AllTogether().to(device=device)
 
 loss_fn = nn.MSELoss()
-optimizer = torch.optim.SGD(model.parameters(), lr=1e-3)
+avg_optimizer = torch.optim.SGD(avg_model.parameters(), lr=1e-3)
 
 
 def train(dataloader, model, loss_fn, optimizer):
@@ -138,6 +173,6 @@ def test(dataloader, model, loss_fn):
 epochs = 50
 for t in range(epochs):
     print(f"Epoch {t+1}\n-------------------------------")
-    train(train_dataloader, model, loss_fn, optimizer)
-    test(test_dataloader, model, loss_fn)
+    train(avg_train_dataloader, avg_model, loss_fn, avg_optimizer)
+    test(avg_test_dataloader, avg_model, loss_fn)
 print("Done!")
